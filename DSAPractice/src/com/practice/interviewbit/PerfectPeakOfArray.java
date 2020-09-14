a288523f8057a449a6ba4d350e01acc24a05387b
nishantbansal <nishantbansal023@gmail.com>
2020-09-14 22:20:05 +0530

    Day 19 14th September

---
+++ DSAPractice/src/com/practice/interviewbit/PerfectPeakOfArray.java
@@ -0,0 +1,33 @@
+package com.practice.interviewbit;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PerfectPeakOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            list.add(i, sc.nextInt());
+        int ans = peakOfArray(list);
+        System.out.println(ans);
+    }
+
+    private static int peakOfArray(ArrayList<Integer> list) {
+        int n = list.size();
+        int[] min = new int[n];
+        min[n-1] = list.get(n-1);
+        for (int i = n-2; i >= 0; i--){
+            min[i] = Math.min(min[i+1], list.get(i));
+        }
+
+        int max = list.get(0);
+        for (int i = 0; i < n; i++){
+            max = Math.max(max, list.get(i));
+            if (i != 0 && i != n-1 && max == min[i])
+                return 1;
+        }
+        return 0;
+    }
+}
